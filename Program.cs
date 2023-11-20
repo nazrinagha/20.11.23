c1a7f456dac91f1df0a0b5142531943417f7343d
Nezrin <johndoe@example.com>
2023-11-20 10:49:36 +0400

    20.11.23

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿//3cu tapshiriq / Daxil edilen ededin faktorialini tapin .
+int a = 6;
+int fact = 1;
+do
+{
+    fact *= a;
+    a--;
+} while (a > 0);
+Console.WriteLine("Factorial ={0}", fact  );
+Console.ReadLine();
+
+
+//Ikinci tapshiriq / input olaraq integer bir deyer gelir,
+//siz bu ededin neche mertebeli oldugunu consolea yazdiririq.
+
+
+
+
+
+//input olaraq ilin aylarinin her hansi biri daxil edilir, 
+//biz onu consolea hansi fesil oldugunu yazdiririq.Yanlish ay adi daxil edildikde, bunu
+//qebul etmemelidir.
+
